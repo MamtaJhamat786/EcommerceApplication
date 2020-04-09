0b15eaf40e80ea025be94aa9715c1478c1f6f50d
mamtajhamat786 <mamtajhamat27@gmail.com>
2020-04-08 17:54:30 -0700

    ecommerce app

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-nav>
+   <h3>E-commerce platform</h3>
+   <b-nav-item to="/">Home  </b-nav-item>
+   <b-nav-item to="/cart">ShoppingCart </b-nav-item>
+   <b-nav-item to="/orderlisting">OrderListing </b-nav-item>
+  </b-nav>
+    
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  
+}
+</script>
+
+<style scoped>
+
+</style>
